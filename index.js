3e676e3841b49237a9d78229c372cf7f55cbc795
Mahbub Noyon <mahbubnoyon506@gmail.com>
2022-04-29 07:34:40 +0600

    Werehouse server setup complete

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9bf1d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('DB connected')
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Warehouse runnibg')
+});
+
+app.listen(port, () => {
+    console.log('Server listening the port', port);
+});
