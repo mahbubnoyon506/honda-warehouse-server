f686a95581c0940fcc379b83ec4a1f3c980a2566
Mahbub Noyon <mahbubnoyon506@gmail.com>
2022-04-30 12:12:44 +0600

    product find by one

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -12,13 +12,35 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9bf1d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('DB connected')
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+
+  await client.connect();
+  const productsCollection=client.db('CarAccessories').collection('parts');
+
+   try{
+    app.get('/products', async(req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const result = await cursor.toArray()
+      res.send(result);
+    })
+     
+    app.get('/products/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    })
+
+
+  
+   }
+   finally{
+
+   }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('Warehouse runnibg')
