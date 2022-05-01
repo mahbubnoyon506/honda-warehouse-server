ed0ce4a2c1e5610ca3fe9ac44abfa5392a6467b0
Mahbub Noyon <mahbubnoyon506@gmail.com>
2022-05-01 18:11:51 +0600

    Some issues solved

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message: 'Unauthorized Access'})
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if(err){
+      return res.status(403).send({message: 'Your access is forbidden'});
+    }
+    req.decoded = decoded;
+    // console.log('decoded', decoded);
+    next()
+  }) 
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9bf1d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -17,7 +33,8 @@ async function run(){
 
   await client.connect();
   const productsCollection=client.db('CarAccessories').collection('parts');
-  const likedItemsCollection = client.db('LikedItems').collection('items')
+  const likedItemsCollection = client.db('LikedItems').collection('items');
+  const emailCollection = client.db('NewsEmail').collection('Emails')
 
    try{
     app.get('/products', async(req, res) => {
@@ -46,7 +63,20 @@ async function run(){
       const result = await productsCollection.deleteOne(query);
       res.send(result);
     })
-
+    //update 
+    app.put('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const query = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+          $set: {
+              quantity: data.quantity,
+          }
+      };
+      const result = await serviceCollection.updateOne(query, updateDoc, options);
+      res.send(result);
+  })
     //// Visitor loved item
     app.post('/items', async(req, res) => {
       const newItem = req.body;
@@ -54,12 +84,18 @@ async function run(){
       res.send(result);
     })
 
-    app.get('/items', async(req, res) => {
+    app.get('/items', verifyJWT, async(req, res) => {
+      // const decodedEmail = req.decoded.email;
       const email = req.query.email;
-      const query = { email: email };
-      const cursor = likedItemsCollection.find(query);
-      const result = await cursor.toArray();
-      res.send(result);
+      // if(email === decodedEmail){
+        const query = { email: email };
+        const cursor = likedItemsCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+      // }
+      // else{
+      //   res.status(403).send({message: 'Your access is forbidden'})
+      // }
     })
 
     //item deletion
@@ -70,6 +106,22 @@ async function run(){
       res.send(result);
     })
   
+    // Auth validation
+    app.post('/login', async(req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: '1d'
+      });
+      res.send({accessToken});
+    })
+
+    //Newsletter info
+    app.post('/newsletters', async(req, res) => {
+      const newEmail = req.body;
+      const result = await emailCollection.insertOne(newEmail);
+      res.send(result);
+    })
+
    }
    finally{
 
